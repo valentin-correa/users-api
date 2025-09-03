9be862c870e4c7b8c8b42ec956bc7690f59d8788
Mateo Letona <mateolet883@gmail.com>
2025-09-03 14:18:57 -0300

    Sumo nombre y apellido al user

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -7,6 +7,12 @@ export class UserEntity extends BaseEntity implements UserI {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
   @Index({unique:true})
   @Column()
   email: string;
--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,8 @@
 import { RoleEntity } from "src/entities/role.entity";
 
 export interface UserI {
+  firstname : string;
+  lastname: string;
   email: string;
   password: string;
   role: RoleEntity;
